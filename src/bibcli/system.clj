1fa5da0d6f3d92dab8df66d45838ec3f3360d57f
ptrLx <13c@posteo.de>
2022-12-12 13:23:52 +0000

    system docstrings

--- src/bibcli/system.clj
+++ src/bibcli/system.clj
@@ -5,50 +5,67 @@
 
 ;;;; General project
 
-(defn init_central []
-  ;; Add initial folder structure
-  (fs/create-dirs (str (fs/home) "/.bibcli/res")))
+(def root_folder
+  (str (fs/home) "/.bibcli"))
+(def config_json
+  (str (fs/home) "/.bibcli.json"))
 
-(defn init_config []
-  ;; Create an empty(!) config file
-  (fs/create-file (str (fs/home) "/.bibcli.conf")))
+(defn init_central
+  "Add initial folder structure"
+  []
+  (fs/create-dirs (str (root_folder) "/res")))
 
-(defn delete_central []
-  ;; Delete initial folder structure
-  (fs/delete-tree (str (fs/home) "/.bibcli")))
+(defn init_config
+  "Create an empty(!) config file"
+  []
+  (fs/create-file config_json))
 
-(defn list_aliases []
-  ;; Return a list with names of all available aliases
-  ;; Check project directory exists
-  (if (fs/exists? (str (fs/home) "/.bibcli/res"))
-    (let [FILTER_FOLDER (map str (filter fs/directory? (fs/list-dir (str (fs/home) "/.bibcli/res"))))]
-      (map #(clojure.string/replace %1 (str (fs/home) "/.bibcli/res/") "") FILTER_FOLDER))
+(defn delete_central
+  "Delete initial folder structure"
+  []
+  (fs/delete-tree root_folder))
+
+(defn list_aliases
+  "Return a list with names of all available aliases
+   Check project directory exists"
+  []
+  (if (fs/exists? (str (root_folder) "/res"))
+    (let [FILTER_FOLDER (map str (filter fs/directory? (fs/list-dir (str (root_folder) "/res"))))]
+      (map #(clojure.string/replace %1 (str (root_folder) "/res/") "") FILTER_FOLDER))
     nil))
 
 ;;;; Handle config content
 
-(defn read_config []
-  ;; Return a dictionary from json config
-  (json/read (clojure.java.io/reader (str (fs/home) "/.bibcli.conf"))))
+(defn read_config
+  "Return a dictionary from json config"
+  []
+  (json/read (clojure.java.io/reader config_json)))
 
-;; Return all keys from config
-(defn keys_of_config [] (keys (read_config)))
+(defn keys_of_config
+  "Return all keys from config"
+  []
+  (keys (read_config)))
 
-;; Get value from key
-(defn val_from_key_config [key] ((read_config) key))
+(defn val_from_key_config
+  "Get value from key"
+  [key]
+  ((read_config) key))
 
-;; Write with arg as map to config
-(defn write-config [arg]
+(defn write-config
+  "Write with arg as map to config"
+  [arg]
   (if (map? arg)
-    (fs/write-lines (fs/file (str (fs/home) "/.bibcli.conf")) [(json/write-str arg :indent true)])
+    (fs/write-lines (fs/file config_json) [(json/write-str arg :indent true)])
     nil))
 
-;; Add key-value pair to config json
-(defn add_data_config [key value]
+(defn add_data_config
+  "Add key-value pair to config json"
+  [key value]
   (write-config (assoc (read_config) key value)))
 
-;; Delete by key of config json
-(defn del_data_config [key]
+(defn del_data_config
+  "Delete by key of config json"
+  [key]
   (let [readMap (read_config)]
     (if (readMap key) (write-config (dissoc readMap key)) nil)))
 
@@ -59,7 +76,7 @@
 
 (defn alias_exists? [alias]
   ;; Return true or false
-  (path_valid? (str (fs/home) "/.bibcli/res/" alias)))
+  (path_valid? (str (root_folder) "/res/" alias)))
 
 ;;;; Bibtex related
 
@@ -69,12 +86,24 @@
   ;; todo
   true)
 
-(expound/def ::BIB-REF-EXISTS
+(e/def ::BIB-REF-EXISTS
   (fn [_outfile]
     (bib-ref_exists?))
-  "no bib-ref file found in current folder")
+  "No bib-ref file found in current folder")
 
-;; todo (spec/def ::CAN-INIT (spec/and (spec/not ::BIB-REF-EXISTS) (LIST-ALIAS-EXISTS)))
+(e/def ::BIB-NOT-REF-EXISTS
+  (fn [_outfile]
+    (not (bib-ref_exists?)))
+  "Bib-ref file already exists in current folder. Use `bibcli add` instead")
+
+(s/def ::CAN-INIT
+  (s/and (vector? ::BIB-NOT-REF-EXISTS)))
+
+;; (e/def ::CAN-INIT
+;;   (fn [a]
+;;     (println a)
+;;     true)
+;;   "test")
 
 (defn create_bib-ref
   "Write aliases per line to ./bib-ref"
@@ -118,6 +147,7 @@
 ;;;; NOT IMPLEMENTED
 
 ;; COMMENT_MB: Das fällt unter Kategorie Bibtex?
+
 (defn list_all_res_from_author
   "Scan all resources and return a set of aliases for given author.
    only include resources of this type, if type is not nil"
@@ -126,6 +156,7 @@
   )
 
 ;; COMMENT-MB: Ist das notwendig?
+
 (defn get_path
   "Return path of the resource of a given alias"
   [alias]
@@ -133,6 +164,7 @@
   )
 
 ;; DUMMY-FUNCTIONS TO GRANT FUNCTIONALLITY
+
 (defn set_autocommit []
   (do))
 
